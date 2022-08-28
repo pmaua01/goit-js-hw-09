ad88c278178f3feec426bae61c1deef45b88be0a
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-08-28 12:07:39 +0300

    end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    bottomStart:document.querySelector("[data-start]"),
+    bottomStop:document.querySelector("[data-stop]"),
+    body:document.body
+}
 
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+refs.bottomStart.addEventListener("click",onClick)
+
+refs.bottomStop.addEventListener("click", onRemove)
+
+
+function onClick (){
+    timerId = setInterval(()=>{
+        refs.body.style.background=getRandomHexColor() 
+    },1000)
+    console.log(timerId)
+   
+    refs.bottomStart.disabled= true;
+}
+
+function onRemove(){
+ 
+    refs.bottomStart.disabled= false;
+    clearInterval(timerId)
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,89 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+refs = {
+  amountBtn: document.querySelector('input[name="amount"]'),
+  startBtn: document.querySelector('button'),
+  form: document.querySelector('.form'),
+  allInput: document.querySelectorAll('input'),
+};
+
+// console.log(refs.amountBtn);
+// console.log(refs.startBtn);
+// console.log(refs.form);
+// console.log(refs.allInput);
+
+refs.startBtn.addEventListener('click', onInput);
+
+function onInput(e) {
+  // const formInput = {};
+  // formInput[e.target.name] = e.target.value;
+  // console.log(formInput);
+  e.preventDefault();
+  let inputValue = {};
+  refs.allInput.forEach(element => {
+    // console.log(element.name, Number(element.value));
+    inputValue[element.name] = Number(element.value);
+
+    // element.name = element.value;
+  });
+  // console.log('Input value', inputValue);
+
+  let fullDelay = inputValue.delay;
+  let count = 1;
+  // console.log(fullDelay);
+  for (let i = 0; i < inputValue.amount - 1; i += 1) {
+    count += 1;
+
+    if (i === 0) {
+      position = 1;
+
+      fullDelay = fullDelay;
+      // console.log(fullDelay);
+      createPromise(position, fullDelay)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+    fullDelay += inputValue.step;
+    // console.log(fullDelay);
+    createPromise(count, fullDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+  //
 }
+
+function createPromise(position, delay) {
+  // console.log('Run Fn', position, delay);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+// function craeteFn({ amount, step, delay }) {
+//   console.log('This is amount', amount);
+//   console.log('This is step', step);
+//   console.log('This is delay', delay);
+// }
